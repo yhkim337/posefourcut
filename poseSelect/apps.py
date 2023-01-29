2c2eb1495c7b7ece87810651df74df72de68eda7
yhkim <yhkim4433@gmail.com>
2023-01-30 00:32:40 +0900

    poseSelect first page

---
+++ poseSelect/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PoseselectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'poseSelect'
---
+++ posefourcut/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+import poseSelect.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', poseSelect.views.index, name='index'),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

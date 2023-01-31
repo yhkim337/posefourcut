b7ee1a1be9a891d3716478aa8ab6e34389d7b387
yhkim <yhkim4433@gmail.com>
2023-01-31 23:44:07 +0900

    url connect

--- poseSelect/views.py
+++ poseSelect/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'poseSelect/index.html')
+
+def fourbyone(request):
+    return render(request, 'poseSelect/fourbyone.html')
--- posefourcut/urls.py
+++ posefourcut/urls.py
@@ -7,5 +7,6 @@ import poseSelect.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', poseSelect.views.index, name='index'),
+    path('fourbyone/', poseSelect.views.fourbyone, name='fourbyone'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

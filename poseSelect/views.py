d63174230bea3057f3ac4c43787e659160ccda4c
yhkim <yhkim4433@gmail.com>
2023-02-07 22:16:11 +0900

    3rd page css start

--- poseSelect/views.py
+++ poseSelect/views.py
@@ -8,3 +8,15 @@ def fourbyone(request):
 
 def twobytwo(request):
     return render(request, 'poseSelect/twobytwo.html')
+
+def fourbyone_1(request):
+    return render(request, 'poseSelect/fourbyone_1.html')
+
+def fourbyone_2(request):
+    return render(request, 'poseSelect/fourbyone_2.html')
+
+def fourbyone_3(request):
+    return render(request, 'poseSelect/fourbyone_3.html')
+
+def fourbyone_4(request):
+    return render(request, 'poseSelect/fourbyone_4.html')
--- posefourcut/urls.py
+++ posefourcut/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('', poseSelect.views.index, name='index'),
     path('fourbyone/', poseSelect.views.fourbyone, name='fourbyone'),
     path('twobytwo/', poseSelect.views.twobytwo, name='twobytwo'),
+    path('fourbyone_1/', poseSelect.views.fourbyone_1, name='fourbyone_1'),
+    path('fourbyone_2/', poseSelect.views.fourbyone_2, name='fourbyone_2'),
+    path('fourbyone_3/', poseSelect.views.fourbyone_3, name='fourbyone_3'),
+    path('fourbyone_4/', poseSelect.views.fourbyone_4, name='fourbyone_4'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

e5d104e8cf78b04f0790f67147d7666edf78ec96
yhkim <yhkim4433@gmail.com>
2023-02-22 00:11:35 +0900

    apply.js start

---
+++ poseSelect/static/js/apply.js
@@ -0,0 +1,5 @@
+console.log("사진변경준비")
+function toggleImg() {
+    // document.getElementById("img").src = "images/db/fourbyone/sample.png";
+    console.log(document.querySelector(".modal_content"));
+  }
